96bc10c590e7d32624f46546def69cbb2b08d592
Osmar CS <osmar.costa.s+tson@gmail.com>
2018-04-22 23:32:27 -0300

    feature: filter pokemom list

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,19 +1,26 @@
 import Controller from '@ember/controller';
+import  { computed } from '@ember/object';
 
 export default Controller.extend({
+  pkmFilter: '',
+  pkmList: computed('list', 'pkmFilter', function() {
+    const pkmList = this.get('list');
+    const pkmFilter = this.get('pkmFilter').toLowerCase();
+    return pkmList.filter(pkm => pkm.name.includes(pkmFilter));
+  }),
   init() {
     this._super(...arguments);
-    this.pkmList = [
+    this.list = [
       {
-        name: 'Pikachu',
+        name: 'pikachu',
         number: 25
       },
       {
-        name: 'Chikorita',
+        name: 'chikorita',
         number: 152
       },
       {
-        name: 'Blaziken',
+        name: 'blaziken',
         number: 257
       }
     ]
